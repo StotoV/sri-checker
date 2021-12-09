3914c47b8d5cc670af8dcf83eb055c4f2cf06ab7
Tom Stock <info@tomstock.nl>
2021-12-09 11:02:42 +0100

    Minor improvement of CLI:

--- src/index.js
+++ src/index.js
@@ -29,23 +29,39 @@ const fs = require('fs');
 /**
  * Runs the full SRI check on the given URL
  *
- * @param    {string}    URL        The URL that will be checked on SRI implementation
+ * @param    {string}    target     The URL that will be checked on SRI implementation
  * @param    {string}    outPath    The location where the results will be written. Must be a JSON file
  *
  * @TODO             Add input validation on params
  * @TODO             Create destination dir if it does not exist
  */
-async function checkURL (URL, outPath) {
-    logger.verbose('checkURL called with URL: ' + URL + ' and outPath: ' + outPath)
+async function checkURL (target, outPath) {
+    logger.verbose('checkURL called with URL: ' + target + ' and outPath: ' + outPath)
 
-    const scrapeResult = await scrape(URL)
-    // await fs.writeFileSync(outPath + 'scrape.json', out);
+    try {
+        const u = new URL(target)
+        if (u.protocol !== 'http:' && u.protocol !== 'https:') {
+            throw 'Invalid protocol for given target.'
+        }
+    } catch (e) {
+        logger.error('URL could not be parsed: ' + e)
+        process.exit(1)
+    }
+
+    try {
+        fs.mkdirSync(outPath, { recursive: true })
+    } catch (e) {
+        logger.error('Failed to find or create directory: ' + e)
+        process.exit(1)
+    }
+
+    const scrapeResult = await scrape(target)
+    await fs.writeFileSync(outPath + '/scrape.json', JSON.stringify(scrapeResult, null, 2));
 
     const labelResult = await label(scrapeResult)
-    logger.verbose(JSON.stringify(labelResult, null, 2))
-    // await fs.writeFileSync(outPath + 'label.json', out);
+    await fs.writeFileSync(outPath + '/label.json', JSON.stringify(labelResult, null, 2));
 
-    logger.verbose('Done with all tests for URL: ' + URL)
+    logger.verbose('Done with all tests for URL: ' + target)
     process.exit(0)
 }
 
